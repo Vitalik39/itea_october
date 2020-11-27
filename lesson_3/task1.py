289e10d0570a115266265788d0713c0e3ea99c98
Vitalik <vitaliksemenchuk0@gmail.com>
2020-11-28 00:06:43 +0200

    ''

---
+++ lesson_3/task1.py
@@ -0,0 +1,27 @@
+import time
+
+def decorator(number=3):
+
+    def actual_decorator(func):
+
+        def wrapper():
+            Call = []
+            i = 0
+            sum_ = 0
+            while i < number:
+                start_time = time.time()
+                result = func()
+                end_time = time.time()
+                Call.append(end_time - start_time)
+                sum_ += (end_time - start_time)
+                i += 1
+            res = {
+                'Every_Call': Call,
+                'Total_Time': sum_,
+                'Name': func.__name__,
+                'Result': func()
+            }
+            return res
+        return wrapper
+
+    return actual_decorator
