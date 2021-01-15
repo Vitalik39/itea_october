43d8d9f3661a7a91ac288a7d09dc84699637c125
Vitalik <vitaliksemenchuk0@gmail.com>
2021-01-16 00:32:34 +0200

    Add task1

---
+++ lesson_7/task1.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+class DataConn:
+
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
+
+    def __exit__(self):
+        self.conn.close()
